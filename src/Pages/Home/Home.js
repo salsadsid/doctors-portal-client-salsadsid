53d4b3314644abcf16a17042c00a3bc5605e45db
salsadsid <salman.dnj@gmail.com>
2022-05-14 01:31:23 +0600

    doctors-portal-project-client-side-initialization

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../Shared/Navbar';
+import Banner from './Banner';
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default Home;
